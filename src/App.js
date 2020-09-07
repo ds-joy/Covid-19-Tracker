281db556761b9cf5f70258b74b75cbb7cfa09580
Debasish Sarker <debasishsarker36@gmail.com>
2020-09-07 23:22:35 +0600

    created the components { Cards, Chart, CountryPicker }

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
 
+import { Cards, Chart, CountryPicker } from './components';
+import { fetchData } from './Api'
+import styles from './App.module.css';
+
+
+
 class App extends React.Component {
+
+    // bringing in the data
+    // to make the componentDidMount we need to
+    // put 'async keyword in front of the function'
+    async componentDidMount() {
+        const data = await fetchData()
+
+        console.log(data)
+
+    }
+
     render() {
         return (
-            <div>
-                <h1>App</h1>
+            <div className={styles.container}>
+                <Cards />
+                <CountryPicker />
+                <Chart />
             </div>
         )
     }
 }
+
+export default App
